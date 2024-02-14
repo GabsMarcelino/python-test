75fcd3cab4de2b78195bcc9cefa1c90085241597
Gabriel Marcelino <gmarcelino@tractian.com>
2024-02-14 10:52:31 -0300

    improve: code coverage

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,6 +8,9 @@ class MainTest(unittest.TestCase):
         assert add(2, 3) == 5
         assert add(2, 4) == 6
 
+    def test_subtract(self):
+        assert subtract(2, 3) == -1
+        assert subtract(2, 4) == -2
 
 if __name__ == '__main__':
     unittest.main()
